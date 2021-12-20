b589008c8944e42b92fff6ea8ea7c0c3766cb73a
Annee11333 <91998020+Annee11333@users.noreply.github.com>
2021-12-19 19:45:02 -0500

    Update UserController.java

--- src/main/java/com/usa/ciclo4/reto3_4a/controller/UserController.java
+++ src/main/java/com/usa/ciclo4/reto3_4a/controller/UserController.java
@@ -51,7 +51,7 @@ public class UserController {
     public  boolean emailExists(@PathVariable("email")String email){
         return  userService.emailExists(email);
     }
-    @GetMapping("/birthtday/{month}")
+    @GetMapping("/birthday/{month}")
     public List<User> getByMonthBirthtDay(@PathVariable("month") String month){
         return userService.getByMonthBirthtDay(month);
     }
